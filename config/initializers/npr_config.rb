78a6d130d2f27e322106e8e8324880de1675b775
bryan <bryan@SWAGPC.localdomain>
2016-07-20 23:17:29 -0400

    Initialize Rails project

---
+++ config/initializers/npr_config.rb
@@ -0,0 +1,5 @@
+NPR.configure do |config|
+  config.apiKey = "MDEwNjQyNzU3MDEzNTY3MTI3MzM5ZjUxNg001" # Codecademy API key, use own asap!
+  config.sort = 'date descending'
+  config.requiredAssets = 'text'
+end
