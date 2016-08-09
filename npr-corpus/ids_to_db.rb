776c4fd295c31775848a65065a18857d8ca2098e
Bryan Li <bryanli.ca@gmail.com>
2016-08-09 02:12:12 -0400

    Update scripts for rubocop style, unix file endings

--- npr-corpus/ids_to_db.rb
+++ npr-corpus/ids_to_db.rb
@@ -1,22 +1,22 @@
 class IdsToDb
   API_KEY = ENV['NPR_API_KEY']
-  
+
   attr_reader :array, :file_name
-  
+
   def initialize(client, file_name)
     @client = client
     @file_name = file_name
   end
 
   def array_from_id_file
-	return @array if @array
+    return @array if @array
     @array = []
     File.open(@file_name, 'rb').readlines.each do |line|
-      @array.push(*line.split(" "))
+      @array.concat(line.split(' '))
     end
     @array
   end
-  
+
   def write_to_database
     ids = array_from_id_file
     count = 0
@@ -26,7 +26,7 @@ class IdsToDb
         paragraphs = xml['paragraph'].map(&:squish)
         array = get_title_date_and_audio_link(id)
         puts "Adding story #{id}"
-        
+
         transcript = Transcript.new(
           story_id: id,
           title: array.first,
@@ -43,35 +43,38 @@ class IdsToDb
     end
     puts "Added #{count} transcripts to database"
   end
-  
+
   def parse_xml(id)
-      uri = URI.parse("http://api.npr.org/transcript?id=#{id}&apiKey=#{API_KEY}")
-      xml_data = Net::HTTP.get_response(uri).body
-      Hash.from_xml(xml_data)['transcript']
+    uri = URI.parse("http://api.npr.org/transcript?id=#{id}&apiKey=#{API_KEY}")
+    xml_data = Net::HTTP.get_response(uri).body
+    Hash.from_xml(xml_data)['transcript']
   end
-  
+
   def get_title_date_and_audio_link(id)
     story = @client.query(
       fields: 'title,storyDate,audio',
       id: id
     ).list.stories.first
-    
+
     title = story.title
     date = story.storyDate
     audio_link = get_audio_link(story.audio)
     Rails.logger.error "Audio not found for #{id}" if audio_link.nil?
     [title, date, audio_link]
   end
-  
+
   def get_audio_link(audio_array)
     case audio_array.size
     when 0
       nil
     when 1
-      audio_array.first.formats.mp3s.first.content + '&dl=1' rescue nil
+      mp3 = audio_array.first.formats.mp3s.first
+      return nil unless mp3
+      mp3.content.sub(/\?.*/, '?dl=1')
     else
       index = audio_array.map(&:type).index('primary')
-      audio_array[index].formats.mp3s.first.content.sub(/\?.*/, "?dl=1") rescue nil
+      mp3 = audio_array[index].formats.mp3s.first
+      mp3.content.sub(/\?.*/, '?dl=1')
     end
   end
 end
