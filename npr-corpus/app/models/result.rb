dccc8fe551c8982fdeb2decd92534008ae37771e
Bryan Li <bryanli.ca@gmail.com>
2016-08-31 20:22:33 -0700

    Reduce size of result, allow destroy of searches in controller

--- npr-corpus/app/models/result.rb
+++ npr-corpus/app/models/result.rb
@@ -1,14 +1,12 @@
 class Result
   include Mongoid::Document
-  field :transcript_id, type: String
-  field :title, type: String
-  field :url_link, type: String
-  field :audio_link, type: String
-  field :context, type: String
-  field :paragraph, type: String # TODO: remove fields besides paragraph and use index for context and follows
-  field :follow1, type: String
-  field :follow2, type: String
   field :sentence, type: String
+  field :paragraph_index, type: Integer
 
+  belongs_to :transcript
   belongs_to :search
+
+  def paragraph
+    self.transcript.paragraphs[paragraph_index]
+  end
 end
